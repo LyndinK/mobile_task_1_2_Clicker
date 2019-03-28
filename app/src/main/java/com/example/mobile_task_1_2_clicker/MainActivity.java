eaf8a908d2c8cb8cd63f04b6fc451bc0701cfdab
Kirill_Lyndin <beldima@gmail.com>
2019-03-28 12:34:02 +0300

    сделяль

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
     Button mainBtn;
+    Button lessBtn;
+    Button ann;
+    ImageButton KIT;
 
     private long score = 0;
 
@@ -21,18 +25,54 @@ public class MainActivity extends AppCompatActivity {
 
         mainText = (TextView) findViewById(R.id.mainText);
         mainBtn = (Button) findViewById(R.id.button);
+        lessBtn = (Button) findViewById(R.id.button2);
+        ann = (Button) findViewById(R.id.button3);
+        KIT = (ImageButton) findViewById(R.id.imageButton3);
 
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 score ++;
-                String s = "Кликов: " + score;
+                String s = "" + score;
                 mainText.setText(s.toCharArray(),0, s.length());
             }
         };
 
         mainBtn.setOnClickListener(clickListener);
+
+        View.OnClickListener clickListener1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score --;
+                String s = "" + score;
+                mainText.setText(s.toCharArray(),0, s.length());
+            }
+        };
+
+        lessBtn.setOnClickListener(clickListener1);
+
+        View.OnClickListener clickListener2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
+                String s = "" + score;
+                mainText.setText(s.toCharArray(),0, s.length());
+            }
+        };
+
+        ann.setOnClickListener(clickListener2);
+
+        View.OnClickListener clickListener3 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
+                String s = "" + score;
+                mainText.setText(s.toCharArray(),0, s.length());
+            }
+        };
+
+        KIT.setOnClickListener(clickListener3);
 
     }
 }
